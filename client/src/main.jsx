192fad7f3a9c688314466f97b57098204d08a1d3
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-11 19:42:03 +0300

    update App.js and main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
+import { store, persistor } from "./redux/app/store";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
+
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </ReduxProvider>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      // theme={mode === "dark" ? "dark" : "light"}
+      limit={3}
+    />
   </StrictMode>
 );
