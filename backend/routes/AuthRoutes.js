061bbe49937a1ae3c0277dc0ab6d162f7efb27bb
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-12 22:35:26 +0300

    auth routes: add verify, reset and forgot password

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { signup, verifyEmail, login } from "../controllers/AuthController.js";
+import {
+  signup,
+  verifyEmail,
+  login,
+  logout,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/AuthController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/verify/:token", verifyEmail);
+router.post("/verify-email", verifyEmail);
 router.post("/login", login);
+router.get("/logout", logout);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:resetToken", resetPassword);
 
 export default router;
