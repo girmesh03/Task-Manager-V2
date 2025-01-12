ed7272f896612ad3aa6bd7e183eb1a323972947d
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-12 22:36:33 +0300

    utils: add email templates, update generate token and send email

--- backend/utils/SendEmail.js
+++ backend/utils/SendEmail.js
@@ -1,24 +1,82 @@
 import nodemailer from "nodemailer";
+import {
+  VERIFICATION_EMAIL_TEMPLATE,
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+} from "./EmailTemplates.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    const mailOptions = {
+      from: `"${process.env.APP_NAME}" <${process.env.SMTP_USER}>`,
+      to: email,
+      subject: "Verify Your Email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    };
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new Error(`Error sending verification email: ${error}`);
+  }
+};
 
-  const verificationLink = `${process.env.CLIENT_URL}/verify/${verificationToken}`;
-  const mailOptions = {
-    from: `"${process.env.APP_NAME}" <${process.env.SMTP_USER}>`,
-    to: email,
-    subject: "Verify Your Email",
-    html: `<p>Click the link below to verify your email:</p>
-           <a href="${verificationLink}">${verificationLink}</a>`,
-  };
+export const sendResetPasswordEmail = async (email, resetURL) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    const mailOptions = {
+      from: `"${process.env.APP_NAME}" <${process.env.SMTP_USER}>`,
+      to: email,
+      subject: "Reset Your Password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    };
 
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
+
+export const sendRestSuccessEmail = async (email) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    const mailOptions = {
+      from: `"${process.env.APP_NAME}" <${process.env.SMTP_USER}>`,
+      to: email,
+      subject: "Password Reset Successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    };
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new Error(`Error sending password reset success email: ${error}`);
+  }
 };
