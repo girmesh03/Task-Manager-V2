c89947d187c566af79db68ac8b9c1fc919d70fef
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-12 22:34:38 +0300

    update user controller setup for getting all and single user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,11 +5,24 @@ import User from "../models/UserModel.js";
 // @desc Get users
 // @route GET /api/users
 // @access Private
-const getUsers = asyncHandler(async (req, res, next) => {});
+const getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find({}).sort({ createdAt: -1 });
+  res.status(200).json(users);
+});
 
 // @desc Get user
 // @route GET /api/users/:userId
 // @access Private
-const getUser = asyncHandler(async (req, res, next) => {});
+const getUser = asyncHandler(async (req, res, next) => {
+  const { userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return next(new CustomError("User not found", 404));
+  }
+
+  res.status(200).json(user);
+});
 
 export { getUsers, getUser };
