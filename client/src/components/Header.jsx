da91f0c7ace85d718c0ca41429872cff52d557dc
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-14 20:33:46 +0300

    all neccessary components added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,43 @@
-const Header = () => {
-  return <div>Header</div>;
+import PropTypes from "prop-types";
+import Stack from "@mui/material/Stack";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+
+import ColorModeIconDropdown from "../shared-theme/ColorModeIconDropdown";
+import CustomDatePicker from "./CustomDatePicker";
+import NavbarBreadcrumbs from "./NavbarBreadcrumbs";
+import MenuButton from "./MenuButton";
+import Search from "./Search";
+
+const Header = ({ setSearchText, setSelectedDate }) => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        display: { xs: "none", md: "flex" },
+        width: "100%",
+        alignItems: { xs: "flex-start", md: "center" },
+        justifyContent: "space-between",
+        maxWidth: { sm: "100%", md: "1700px" },
+        pt: 1.5,
+      }}
+      spacing={2}
+    >
+      <NavbarBreadcrumbs />
+      <Stack direction="row" sx={{ gap: 1 }}>
+        <Search setSearchText={setSearchText} />
+        <CustomDatePicker setSelectedDate={setSelectedDate} />
+        <MenuButton showBadge aria-label="Open notifications">
+          <NotificationsRoundedIcon />
+        </MenuButton>
+        <ColorModeIconDropdown />
+      </Stack>
+    </Stack>
+  );
+};
+
+Header.propTypes = {
+  setSearchText: PropTypes.func.isRequired,
+  setSelectedDate: PropTypes.func.isRequired,
 };
 
 export default Header;
--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -1,5 +1,93 @@
+import { styled } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import MuiDrawer, { drawerClasses } from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import MenuContent from "./MenuContent";
+import CardAlert from "./CardAlert";
+import OptionsMenu from "./OptionsMenu";
+import CustomLogo from "./CustomLogo";
+
+import { useSelector } from "react-redux";
+
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer)({
+  width: drawerWidth,
+  flexShrink: 0,
+  boxSizing: "border-box",
+  mt: 10,
+  [`& .${drawerClasses.paper}`]: {
+    width: drawerWidth,
+    boxSizing: "border-box",
+  },
+});
+
 const SideMenu = () => {
-  return <div>SideMenu</div>;
+  const { currentUser } = useSelector((state) => state.auth);
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        display: { xs: "none", md: "block" },
+        [`& .${drawerClasses.paper}`]: {
+          backgroundColor: "background.paper",
+          overflow: "hidden",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 1,
+          mt: "calc(var(--template-frame-height, 0px) + 4px)",
+          p: 1.5,
+        }}
+      >
+        <CustomLogo />
+        <Typography variant="h4" component="h1" sx={{ color: "text.primary" }}>
+          Taskmanager
+        </Typography>
+      </Box>
+      <Divider />
+      <MenuContent />
+      <CardAlert />
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        sx={{
+          p: 1,
+          gap: 1,
+          alignItems: "center",
+          borderTop: "1px solid",
+          borderColor: "divider",
+        }}
+      >
+        <Avatar
+          sizes="small"
+          alt={`${currentUser?.firstName} ${currentUser?.lastName}`}
+          // src="/static/images/avatar/7.jpg"
+          sx={{ width: 36, height: 36 }}
+        />
+        <Box sx={{ overflow: "hidden", textOverflow: "ellipsis" }}>
+          <Typography
+            variant="body2"
+            sx={{ fontWeight: 500, lineHeight: "16px" }}
+          >
+            {`${currentUser?.firstName} ${currentUser?.lastName}`}
+          </Typography>
+          <Typography variant="caption" sx={{ color: "text.secondary" }}>
+            {currentUser?.email}
+          </Typography>
+        </Box>
+        <OptionsMenu />
+      </Stack>
+    </Drawer>
+  );
 };
 
 export default SideMenu;
