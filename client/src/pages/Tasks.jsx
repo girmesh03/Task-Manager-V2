2e24800707b629728d7b98a4a34aa2122fa770fc
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-14 20:35:45 +0300

    add Tasks page useOutletContext for test

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,4 +1,10 @@
+import { useOutletContext } from "react-router";
+
 const Tasks = () => {
+  const { searchText, selectedDate } = useOutletContext();
+
+  console.log("tasks searchText", searchText);
+  console.log("tasks selectedDate", selectedDate);
   return <div>Tasks</div>;
 };
 
