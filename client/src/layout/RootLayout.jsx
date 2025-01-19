e7ba07ad8453cddd4167865905e4c77af58c7098
girmesh03 <girmachewzewdie03@gmail.com>
2025-01-19 23:16:54 +0300

    applayout authlayout and rootlayout updated

--- client/src/layout/RootLayout.jsx
+++ client/src/layout/RootLayout.jsx
@@ -9,6 +9,7 @@ import {
   datePickersCustomizations,
   treeViewCustomizations,
 } from "../theme/customizations";
+import { Box } from "@mui/material";
 
 const xThemeComponents = {
   ...chartsCustomizations,
@@ -21,7 +22,31 @@ const RootLayout = (props) => {
   return (
     <AppTheme {...props} themeComponents={xThemeComponents}>
       <CssBaseline enableColorScheme />
-      <Outlet />
+      <Box
+        sx={(theme) => ({
+          width: "100%",
+          maxWidth: { sm: "100%", md: "1700px" },
+          margin: "0 auto",
+          height: "100dvh",
+          overflow: "hidden",
+          "&::before": {
+            content: '""',
+            display: "block",
+            position: "absolute",
+            zIndex: -1,
+            inset: 0,
+            backgroundImage:
+              "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+            backgroundRepeat: "no-repeat",
+            ...theme.applyStyles("dark", {
+              backgroundImage:
+                "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+            }),
+          },
+        })}
+      >
+        <Outlet />
+      </Box>
     </AppTheme>
   );
 };
